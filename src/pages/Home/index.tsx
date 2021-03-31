76bdf57b8aaa457d3b7a61f311493eff90a92164
Aieda <aieda@arabella.ag>
2021-03-31 16:44:26 -0300

    hooks

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,7 +9,7 @@ import { useCart } from '../../hooks/useCart';
 interface Product {
   id: number;
   title: string;
-  price: number;
+  price: number;  
   image: string;
 }
 
@@ -53,12 +53,12 @@ const Home = (): JSX.Element => {
   return (
     <ProductList>
 
-      {cart.map(product => (
+      {products.map(product => (
 
-        <li>
-          <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-          <strong>Tênis de Caminhada Leve Confortável</strong>
-          <span>R$ 179,90</span>
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
           <button
             type="button"
             data-testid="add-product-button"
@@ -66,7 +66,7 @@ const Home = (): JSX.Element => {
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {cartItemsAmount[product.id] || 0} 2
+              {cartItemsAmount[product.id] || 0} 
           </div>
 
             <span>ADICIONAR AO CARRINHO</span>
